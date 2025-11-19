1111fc28e986b374f9c7df94dbdfaa7a8c94896b
Aryan Vij | COMPTIA Security+ <99361907+vijtheprogammer@users.noreply.github.com>
2025-11-19 14:55:46 -0500

    Create calculate_operations.py

---
+++ calculate_operations.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    sum = x+y
+    return sum
+def sub(x, y):
+    return x - y
+def mul(x, y):
+    return x * y
+def div(x, y):
+    if y == 0:
+        print("cannot divide by 0")
+        return
+    return x / y
